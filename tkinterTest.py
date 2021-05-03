a6c9df771b69b666a0586c55c4e36d52be99beb2
jadarodriguez <79866953+jadarodriguez@users.noreply.github.com>
2021-05-03 11:50:09 -0600

    Add files via upload

---
+++ tkinterTest.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+top = Tk()
+
+L1 = Label(top, text="Hellp, world!")
+
+L1.grind(column= 0, row= 1)
+
+E1 = Entry(top, bd = 5)
+
+E1.grind(column = 0, row =2)
+
+top.mainloop()
